0444802e8f01e703f6eebc2d7fd273b24d5a5660
Stevan M. Vågen <stevanvaagen@gmail.com>
2024-11-17 23:07:28 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+      <div>
+          <h1>RLWI-Tools</h1>
+          <p>A collection of useful tools during RLWI-operations</p>
+      </div>
+
+  );
+}
+
+export default App
